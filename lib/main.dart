3bd36e5393f59b6125af2aee7d742945972b0ad0
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-10-20 01:17:22 -0400

    Display some text on the screen

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// Import a helper library from flutter to get content on the screen.
+import 'package:flutter/material.dart';
+
+// Define a main function to run when the app starts.
+void main() {
+  // Create a new text widget to show some text on the screen.
+  var app = MaterialApp(
+    home: Text('Hello World!'),
+  );
+
+  // Place the text widget on the screen.
+  runApp(app);
+}
+
+
+
