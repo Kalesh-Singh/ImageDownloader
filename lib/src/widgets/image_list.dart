a602316421093f7f3d48e80a2bc8adf31c37a203
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-10-25 23:25:00 -0400

    Using the container widget for layout

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -11,8 +11,19 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
         itemCount: images.length,
         itemBuilder: (context, int index) {
-          return Image.network(images[index].url);
+          return buildImageContainer(images[index]);
         },
+    );
+  }
+
+  Widget buildImageContainer(ImageModel image) {
+    return Container(
+      margin: EdgeInsets.all(19.0),
+      padding: EdgeInsets.all(19.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Image.network(image.url),
     );
   }
 }
